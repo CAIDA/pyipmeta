36c70ec3a1461888ff4f550638484d4345a22459
Alistair King <alistair@caida.org>
2018-01-11 11:36:06 -0800

    Add high level ipmeta module [WIP]

---
+++ pyipmeta/pyipmeta.py
@@ -0,0 +1,10 @@
+import _pyipmeta
+
+
+class IpMeta:
+
+    def __init__(self,
+                 provider,
+                 time=None
+                 ):
+        pass
---
+++ test/pyipmeta_test.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import pyipmeta
+
+ipm = pyipmeta.IpMeta("netacq-edge")
+
